2f018df6d77492ebc1f902c8b4d2785f74a0c505
Venantius Sean Ardi Nugroho <seanardi4@gmail.com>
2024-03-03 18:37:44 +0700

    change to gdt load

--- src/kernel.c
+++ src/kernel.c
@@ -3,9 +3,9 @@
 #include "header/kernel-entrypoint.h"
 #include <stdbool.h>
 
-void kernel_setup(void){
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
-    while (true) b+=1;
+void kernel_setup(void)
+{
+    load_gdt(&_gdt_gdtr);
+    while (true)
+        ;
 }
