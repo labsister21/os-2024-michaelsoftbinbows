6ad7c10467ca888814b4cac6055596a65e8d5939
Hanief Nashrullah <13522100@std.stei.itb.ac.id>
2024-03-03 18:42:02 +0700

    chore: remove TODO on gdt.h

--- src/header/cpu/gdt.h
+++ src/header/cpu/gdt.h
@@ -34,7 +34,6 @@ struct SegmentDescriptor {
     uint8_t base_mid;
     uint8_t type_bit                : 4;
     uint8_t non_system              : 1;
-    // TODO : Continue SegmentDescriptor definition
     uint8_t descriptor_privilege    : 2;
     uint8_t segment_present         : 1;
     uint8_t segment_limit           : 4;
