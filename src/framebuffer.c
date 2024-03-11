28806bdeb550151abb86716c29e82562a5b9d42e
Hanief Nashrullah <hanieffatkhan@gmail.com>
2024-03-11 20:16:45 +0700

    fix: fg sama bg kebalik

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -11,7 +11,7 @@
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
     FRAMEBUFFER_MEMORY_OFFSET[(160*row)+(2*col)] = c;
-    FRAMEBUFFER_MEMORY_OFFSET[(160*row)+(2*col)+1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+    FRAMEBUFFER_MEMORY_OFFSET[(160*row)+(2*col)+1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
 }
 
 // void framebuffer_clear(void) {
