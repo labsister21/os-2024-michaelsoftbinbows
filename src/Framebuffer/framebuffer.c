1fcceab2bf8eb129e605e5d94d592d1dd111f6eb
Hanief Nashrullah <hanieffatkhan@gmail.com>
2024-03-12 10:36:00 +0700

    fix: wrong cursor col

--- src/Framebuffer/framebuffer.c
+++ src/Framebuffer/framebuffer.c
@@ -5,20 +5,17 @@
 #include "../header/stdlib/string.h"
 #include "../header/cpu/portio.h"
 
-int framebuffer_position(uint8_t r, uint8_t c){
-    return (160*r)+(2*c);
-}
 
 void framebuffer_set_cursor(uint8_t r, uint8_t c) {
         out(CURSOR_PORT_CMD, 0xE);
-        out(CURSOR_PORT_DATA,((framebuffer_position(r,c) >> 8) & 0x00FF));
+        out(CURSOR_PORT_DATA,((((160*r)+(c)) >> 8) & 0x00FF));
         out(CURSOR_PORT_CMD, 0xF);
-        out(CURSOR_PORT_DATA,framebuffer_position(r,c) & 0x00FF);
+        out(CURSOR_PORT_DATA,((160*r)+(c)) & 0x00FF);
 }
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
-    FRAMEBUFFER_MEMORY_OFFSET[framebuffer_position(row,col)] = c;
-    FRAMEBUFFER_MEMORY_OFFSET[framebuffer_position(row,col)+1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
+    FRAMEBUFFER_MEMORY_OFFSET[(160*row)+(2*col)] = c;
+    FRAMEBUFFER_MEMORY_OFFSET[(160*row)+(2*col)+1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
 }
 
 void framebuffer_clear(void) {
--- src/kernel.c
+++ src/kernel.c
@@ -11,7 +11,19 @@ void kernel_setup(void) {
     pic_remap();
     initialize_idt();
     framebuffer_clear();
-    framebuffer_set_cursor(0, 0);
+    framebuffer_set_cursor(0, 11);
     __asm__("int $0x4");
+    framebuffer_write(0,0,'H',10,0);
+    framebuffer_write(0,1,'e',10,0);
+    framebuffer_write(0,2,'l',10,0);
+    framebuffer_write(0,3,'l',10,0);
+    framebuffer_write(0,4,'o',10,0);
+    framebuffer_write(0,5,' ',10,0);
+    framebuffer_write(0,6,'w',10,0);
+    framebuffer_write(0,7,'o',10,0);
+    framebuffer_write(0,8,'r',10,0);
+    framebuffer_write(0,9,'l',10,0);
+    framebuffer_write(0,10,'d',10,0);
+    framebuffer_write(0,11,'!',10,0);
     while (true);
 }
