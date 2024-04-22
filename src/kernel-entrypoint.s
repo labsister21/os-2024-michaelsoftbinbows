7a349c25940942d8f3ccf135f497069eee10e566
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-22 20:37:26 +0700

    copas doang

--- src/kernel-entrypoint.s
+++ src/kernel-entrypoint.s
@@ -20,7 +20,30 @@ align 4                              ; the code must be 4 byte aligned
     dd CHECKSUM                      ; and the checksum
 
 
-section .text                                  ; start of the text (code) 
+section .text                                  ; start of the text (code)
+global kernel_execute_user_program ; execute initial user program from kernel
+kernel_execute_user_program:
+    mov  eax, 0x20 | 0x3
+    mov  ds, ax
+    mov  es, ax
+    mov  fs, ax
+    mov  gs, ax
+    
+    ; Using iret (return instruction for interrupt) technique for privilege change
+    ; Stack values will be loaded into these register:
+    ; [esp] -> eip, [esp+4] -> cs, [esp+8] -> eflags, [] -> user esp, [] -> user ss
+    mov  ecx, [esp+4] ; Save first (before pushing anything to stack) for last push
+    push eax ; Stack segment selector (GDT_USER_DATA_SELECTOR), user privilege
+    mov  eax, ecx
+    add  eax, 0x400000 - 4
+    push eax ; User space stack pointer (esp), move it into last 4 MiB
+    pushf    ; eflags register state, when jump inside user program
+    mov  eax, 0x18 | 0x3
+    push eax ; Code segment selector (GDT_USER_CODE_SELECTOR), user privilege
+    mov  eax, ecx
+    push eax ; eip register to jump back
+
+    iret 
 loader:                                        ; the loader label (defined as entry point in linker script)
     mov  esp, kernel_stack + KERNEL_STACK_SIZE ; setup stack register to proper location
     call kernel_setup
