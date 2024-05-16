6a4d1e944d6e5f97565e0c9c688f71278600f90e
hannoobz <13522100@std.stei.itb.ac.id>
2024-05-16 22:20:52 +0700

    fix include

--- src/Paging/paging.c
+++ src/Paging/paging.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include "../header/paging/paging.h"
-
+#include <string.h>
 
 __attribute__((aligned(0x1000))) struct PageDirectory _paging_kernel_page_directory = {
     .table = {
